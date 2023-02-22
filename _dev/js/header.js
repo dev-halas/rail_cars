568ca82ec6d2de273c9cb34350f91c91f6e67b72
devkryu <dev.halas@gmail.com>
2023-02-22 19:49:39 +0100

    add header nad hero section

--- _dev/js/header.js
+++ _dev/js/header.js
@@ -4,3 +4,12 @@ const hamburger = document.querySelector('.hamburger')
 hamburger.addEventListener('click', () => {
     header.classList.toggle('--active')
 })
+
+
+
+const pageHeight = window.innerHeight * 0.1;
+
+window.onscroll = () => {
+    let top = window.scrollY;
+    top >= pageHeight ? header.classList.add('scrolled') : header.classList.remove('scrolled');
+};
