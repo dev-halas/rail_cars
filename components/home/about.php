88b1b98efbb64e6909fb7e462a1cf129801ce39e
kamilogrodowczyk <kamil.web.development@gmail.com>
2023-02-23 21:54:34 +0100

    Add About section

---
+++ components/home/about.php
@@ -0,0 +1,78 @@
+<section id="about" class="about-section">
+    <div class="about-wrapper">
+        <div class="about-container">
+            <h2>
+                <span class="textYellow">Poznaj</span> 
+                <span class="textWhite">nas</span>
+            </h2>
+            <p>Spółka Rail-Cars Sp. z o.o. powstała w roku 2014r. z myślą o klientach z sektoru kolejowego. Spółka nastawiona jest na zadowolenie oraz dobro klienta. Oferuje sprzedaż oraz regenerację części kolejowych, w tym głównie zestawów kołowych do wagonów towarowych. Naprawy części oraz zestawów kołowych odbywają się w Zakładzie w miejscowości Krotoszyn. Spółka posiada zestawy obręczowane oraz monoblokowe przystosowane do różnych typów wózków wagonowych. Usługi, które świadczy wykonuje zgodnie z wymogami klienta oraz obowiązującymi przepisami. Rail-Cars Sp. z o.o. posiada własny transport co ułatwia dostarczenie towaru w wybrane przez klienta miejsce.  Dodatkowo spółka Rail-Cars Sp. z o.o. posiada szeroką flotę wagonów towarowych, które wynajmuje pod konkretne potrzeby klienta.  Są to wagony węglarki oraz wagony samowyładowcze do przewozu materiałów sypkich, kruszywa, koksu oraz węgla. W swojej flocie posiada wagony cysterny do przewozu materiałów płynnych. Spółka systematyczne powiększa swoją flotę o nowe pojazdy kolejowe. W nowo otwartym oddziale w Jaworznie odbywają się naprawy wagonów towarowych na wszystkich poziomach utrzymania zgodnie z wszystkimi wymaganiami.</p>
+        </div>
+    </div>
+    <div class="about-vagon-wrapper">
+        <img src="<?php echo THEME_URL; ?>_dev/img/vagon.png" alt="vagon" class="vagonImg">
+    </div>
+    <div class="about-offer-wrapper">
+        <div class="about-offer-container">
+            <div class="about-img-container">
+                <img src="<?php echo THEME_URL; ?>_dev/img/about-regeneration.jpg" alt="zestawy kołowe" class="offerImg">
+                <div class="img-label">
+                    <h4>Regeneracja zestawów kołowych</h4>
+                    <svg class="about-offer-arrow" xmlns="http://www.w3.org/2000/svg" width="36.083" height="19.792" viewBox="0 0 36.083 19.792">
+                        <path id="right-arrow-3" d="M26.187,107.5l-1.607,1.607,7.152,7.152H0v2.273H31.731l-7.152,7.152,1.607,1.607,9.9-9.9Z" transform="translate(0 -107.5)"/>
+                    </svg>
+                </div>
+            </div>
+            <div class="about-img-container">
+                <img src="<?php echo THEME_URL; ?>_dev/img/about-repair-1.jpg" alt="zestawy kołowe" class="offerImg">
+                <div class="img-label">
+                    <h4>Naprawa wagonów kolejowych</h4>
+                    <svg class="about-offer-arrow" xmlns="http://www.w3.org/2000/svg" width="36.083" height="19.792" viewBox="0 0 36.083 19.792">
+                        <path id="right-arrow-3" d="M26.187,107.5l-1.607,1.607,7.152,7.152H0v2.273H31.731l-7.152,7.152,1.607,1.607,9.9-9.9Z" transform="translate(0 -107.5)"/>
+                    </svg>
+                </div>
+            </div>
+            <div class="about-img-container">
+                <img src="<?php echo THEME_URL; ?>_dev/img/about-renting.jpg" alt="zestawy kołowe" class="offerImg">
+                <div class="img-label">
+                    <h4>Wynajem wagonów towarowych</h4>
+                    <svg class="about-offer-arrow" xmlns="http://www.w3.org/2000/svg" width="36.083" height="19.792" viewBox="0 0 36.083 19.792">
+                        <path id="right-arrow-3" d="M26.187,107.5l-1.607,1.607,7.152,7.152H0v2.273H31.731l-7.152,7.152,1.607,1.607,9.9-9.9Z" transform="translate(0 -107.5)"/>
+                    </svg>
+                </div>
+            </div>
+            <div class="about-img-container">
+                <img src="<?php echo THEME_URL; ?>_dev/img/about-repair-2.jpg" alt="zestawy kołowe" class="offerImg">
+                <div class="img-label">
+                    <h4>Naprawa części kolejowych</h4>
+                    <svg class="about-offer-arrow" xmlns="http://www.w3.org/2000/svg" width="36.083" height="19.792" viewBox="0 0 36.083 19.792">
+                        <path id="right-arrow-3" d="M26.187,107.5l-1.607,1.607,7.152,7.152H0v2.273H31.731l-7.152,7.152,1.607,1.607,9.9-9.9Z" transform="translate(0 -107.5)"/>
+                    </svg>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="about-track-wrapper">
+        <img src="<?php echo THEME_URL; ?>_dev/img/track.png" alt="tor" class="trackImg">
+    </div>
+    <div class="about-realisation-wrapper">
+        <div class="about-realisation-container">
+            <div class="white-item"></div>
+            <div class="about-realisation-items">
+                <h2>Realizacje</h2>
+                <div>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="122" height="19.792" viewBox="0 0 122 19.792">
+                        <path id="right-arrow-3" d="M28.1,107.5l-1.635,1.607,7.276,7.152H-83.834v2.273H33.74l-7.276,7.152,1.635,1.607,10.068-9.9Z" transform="translate(38.166 127.292) rotate(180)"/>
+                    </svg>
+                    <svg class="about-realisation-arrow-right" xmlns="http://www.w3.org/2000/svg" width="122" height="19.792" viewBox="0 0 122 19.792">
+                        <path id="right-arrow-3" d="M28.1,107.5l-1.635,1.607,7.276,7.152H-83.834v2.273H33.74l-7.276,7.152,1.635,1.607,10.068-9.9Z" transform="translate(38.166 127.292) rotate(180)"/>
+                    </svg>
+                </div>
+            </div>
+            <div class="about-realisation-images">
+                <img src="<?php echo THEME_URL; ?>_dev/img/realisations-vagon-1.jpg" alt="wagon na torze" class="realisationImg">
+                <img src="<?php echo THEME_URL; ?>_dev/img/realisations-inside.jpg" alt="wagon na torze" class="realisationImg">
+                <img src="<?php echo THEME_URL; ?>_dev/img/realisations-vagon-2.jpg" alt="wagon na torze" class="realisationImg"> 
+            </div>
+        </div>
+    </div>
+</section>
--- front-page.php
+++ front-page.php
@@ -3,6 +3,7 @@
 get_header();
 
     get_template_part('/components/home/hero'); 
+    get_template_part('/components/home/about'); 
 
 get_footer(); 
 
